96d8712991cd6d3e60d7e7127cb59db73e8663d5
prozorovmy <miha.prozorov1@gmail.com>
2024-11-01 14:34:53 +0200

    Коммит pom и запускаемый файл с @SpringBootApplication

---
+++ src/main/java/web/Spring_Boot_311/SpringBoot311Application.java
@@ -0,0 +1,13 @@
+package web.Spring_Boot_311;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot311Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot311Application.class, args);
+	}
+
+}
